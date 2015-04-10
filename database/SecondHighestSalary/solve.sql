297d107f4ae6383dd5e28b6616d21eeef06ffa67
fgp <krystism@gmail.com>
2015-04-10 18:52:17 +0800

    update README.md

--- database/SecondHighestSalary/solve.sql
+++ database/SecondHighestSalary/solve.sql
@@ -5,4 +5,7 @@ insert into Employee values(1, 100);
 insert into Employee values(2, 200);
 insert into Employee values(3, 300);
 */
-select max(Salary) from Employee where Salary < (select max(Salary) from Employee);
+/*
+select max(Salary) as SecondHighestSalary from Employee where Salary < (select max(Salary) from Employee);
+*/
+select ifnull((select distinct Salary from Employee order by Salary desc limit 1,1), null) as SecondHighestSalary;
